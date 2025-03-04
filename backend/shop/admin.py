081065917f2a3ddc95e21c3712070e48a9762d59
Bigbear2006 <morozm982@gmail.com>
2025-03-04 23:17:15 +0300

    made Product.image_tg_id readonly in admin panel

--- backend/shop/admin.py
+++ backend/shop/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from shop import models
 
 admin.site.register(models.Category)
-admin.site.register(models.Product)
 admin.site.register(models.Client)
 admin.site.register(models.Dispatch)
 admin.site.register(models.User)
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ('image_tg_id',)
